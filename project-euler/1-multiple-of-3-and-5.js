77ee698a404b53f887ea71cda0019140c0989055
lord-remi <daniel.dle316@gmail.com>
2020-03-01 22:40:54 +0700

    update project-euler series

---
+++ project-euler/1-multiple-of-3-and-5.js
@@ -0,0 +1,14 @@
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+// The sum of these multiples is 23.
+
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+let total = 0;
+
+for (a = 0; a < 1000; a++) {
+    if ((a % 3 == 0) || (a % 5 == 0)) {
+        total = total + a
+    }
+}
+
+console.log(total)
